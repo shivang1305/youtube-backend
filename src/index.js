c373ec87d360cfbde2c30e4ac40ccabd50bf9aa7
Shivang Sharma <sharmashivang1999@gmail.com>
2024-02-20 23:06:08 +0530

    chore: express server setup is completed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    // check for any error after mongodb connection
+    app.on("error", (err) => {
+      console.log("Error befor MongoDB Connection: ", err);
+      throw err;
+    });
+
+    // listen to the port
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Failed!!!", err);
+  });
