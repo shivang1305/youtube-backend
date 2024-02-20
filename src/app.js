0534c943f602611fa1234e1549c4945e995fd836
Shivang Sharma <sharmashivang1999@gmail.com>
2024-02-21 00:05:19 +0530

    feat: middlewares are added for server config

--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+// cookie parser middleware helps the server to perform CRUD operation on the cookies stored in client browser
+// server can only access those cookies in a secure way and perform the operations on those cookies.
+app.use(cookieParser());
 
 export default app;
