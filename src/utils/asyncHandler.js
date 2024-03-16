23333e35de4ef2970977114783aa84d14763cbe2
Shivang Sharma <sharmashivang1999@gmail.com>
2024-03-16 16:37:39 +0530

    feat: added std formats to handle error and res

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,30 @@
+// HOF structure
+// const func1 = (func2) => {
+//   () => {func2()}
+// }
+
+// this is a higher order func which takes a func as an argument and performs operation on that func
+// we can use this func as a middleware to respond to async functions
+
 const asyncHandler = (requestHandler) => {
   (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
+
+// another way of doing the same thing using try-catch
+
+// const asyncHandler = (requestHandler) => {
+//   async (req, res, next) => {
+//     try {
+//       await requestHandler(req, res, next);
+//     } catch (error) {
+//       res.status(error.code || 500).json({
+//         success: false,
+//         message: error.message,
+//       });
+//     }
+//   };
+// };
 
 export default asyncHandler;
