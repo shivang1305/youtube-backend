8ca86543985feae1c02e39a7476ff9d3f05f9943
Shivang Sharma <sharmashivang1999@gmail.com>
2024-06-01 22:06:22 +0530

    fix: typo to named export

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -23,4 +23,4 @@ const commentSchema = new Schema(
 
 commentSchema.plugin(mongooseAggregatePaginate);
 
-export default Comment = mongoose.model("Comment", commentSchema);
+export const Comment = mongoose.model("Comment", commentSchema);
