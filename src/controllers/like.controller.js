a8ff7cb169f59576b50693d7ec65d63049591611
Shivang Sharma <sharmashivang1999@gmail.com>
2024-06-08 11:57:06 +0530

    feat: toggle comment like api created

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -41,6 +41,45 @@ const toggleLikeVideo = asyncHandler(async (req, res) => {
     );
 });
 
+const toggleLikeComment = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+
+  if (!commentId) throw new ApiError(404, "commentId is missing");
+
+  // find the like object on the given video by the logged in user
+  const isLiked = await Like.findOne({
+    comment: commentId,
+    likedBy: req.user._id,
+  });
+
+  let commentLiked = false;
+
+  if (!isLiked) {
+    // like the video --> toggle like on
+    const like = await Like.create({
+      comment: commentId,
+      likedBy: req.user._id,
+    });
+
+    commentLiked = true;
+
+    if (!like)
+      throw new ApiError(500, "Something went wrong while creating a like");
+  } else {
+    await Like.findByIdAndDelete(isLiked._id); // toggle like off
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { commentLiked },
+        "comment like toggled successfully"
+      )
+    );
+});
+
 // TODO: testing of the api is pending, since the likes are not created in db
 // get all the videos that are liked by the logged in user
 const getLikedVideos = asyncHandler(async (req, res) => {
@@ -73,4 +112,4 @@ const getLikedVideos = asyncHandler(async (req, res) => {
     );
 });
 
-export { toggleLikeVideo, getLikedVideos };
+export { toggleLikeVideo, toggleLikeComment, getLikedVideos };
--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
   getLikedVideos,
+  toggleLikeComment,
   toggleLikeVideo,
 } from "../controllers/like.controller.js";
 
@@ -9,6 +10,7 @@ const router = Router();
 router.use(verifyJWT);
 
 router.route("/video-like/:videoId").post(toggleLikeVideo);
+router.route("/comment-like/:commentId").post(toggleLikeComment);
 router.route("/liked-videos").get(getLikedVideos);
 
 export default router;
