322a436214ec23524331d3570bd5cf2fa95804a5
Shivang Sharma <sharmashivang1999@gmail.com>
2024-06-10 21:12:19 +0530

    fix: allowed all origins for cors

--- src/app.js
+++ src/app.js
@@ -4,12 +4,13 @@ import cors from "cors";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
+const corsOpts = {
+  origin: "*",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"],
+};
+
+app.use(cors(corsOpts));
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
